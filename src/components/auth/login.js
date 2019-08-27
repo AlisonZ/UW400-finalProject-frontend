978c0411439d2e795fb3ea37b7aa9e07092bd1d2
Alison Zerbe <alisonkzerbe@gmail.com>
2019-08-26 18:16:39 -0700

    changes login component to be a form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,7 +3,21 @@ import React from 'react';
 class Login extends React.Component {
     render() {
         return(
-            <div>LOGIN!!!</div>
+           <div>
+                <h1>LOGIN!!</h1>
+               <form>
+                    <div className='login-form-entry'>
+                        <label>Email address</label>
+                        <input></input>
+                   </div>
+                   <div className='login-form-entry'>
+                       <label>Password</label>
+                       <input></input>
+                   </div>
+                   <button type='submit'>Submit</button>
+               </form>
+           </div>
+
         )
     }
 }
