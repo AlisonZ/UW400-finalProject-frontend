d582ad16434ecd886a781401ab8ab1890d9a5097
Alison Zerbe <alisonkzerbe@gmail.com>
2019-08-29 13:47:00 -0700

    Adds re-route on Edit click

--- src/components/assignments/AssignmentView.js
+++ src/components/assignments/AssignmentView.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { withRouter, Redirect, Route } from 'react-router-dom';
 
 import '../../styles/assignmentView.css';
 
 class AssignmentView extends React.Component {
     constructor(props) {
         super(props);
+        this.onEditClick = this.onEditClick.bind(this);
     }
 
+    onEditClick = () => {
+        console.log('ive been cliccck')
+    }
+
+
+
     render(){
+        const Button = withRouter(({ history }) => (
+          <button
+            type='button'
+            onClick={() => { history.push('/edit') }}
+          >
+            Edit
+          </button>
+        ))
         const { title, descr, link, grade } = this.props;
         return (
             <div className="assignmentView-container">
@@ -15,9 +31,12 @@ class AssignmentView extends React.Component {
                 <div className="assignmentView-descr"> {descr}</div>
                 <div className="assignmentView-link">{link} </div>
                 <div className="assignmentView-grade"> {grade} </div>
+                <Button />
+
+                <button>Delete</button>
             </div>
         )
     }
 }
 
-export default AssignmentView;
+export default withRouter(AssignmentView);
